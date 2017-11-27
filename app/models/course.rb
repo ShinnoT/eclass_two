ddf0f8131fae3d22c433f59d80c01ebd21645c0f
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-11-27 23:27:35 +0900

    association changed for course and user

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
+  has_many :users
   belongs_to :teacher
   validates :name, :course_key, presence: true
   validates_uniqueness_of :name, :course_key
