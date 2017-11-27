53ca659dddc3089ec170cad4c344998c45b444d7
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-11-27 17:56:25 +0900

    show action defined in courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,12 @@ class CoursesController < ApplicationController
     # @loggedin_user = current_user || current_teacher
     @courses = Course.all
   end
+
+  def show
+    @course = Course.find(id: params[:id])
+  end
+
+  def new
+
+  end
 end
