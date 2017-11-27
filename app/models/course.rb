9f664614c060cbe98ceb32d5f3c577a064bcc936
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-11-28 00:15:10 +0900

    associations changed for many to many relationship between user and teachers AND also changed logic of enroll action in courses controller

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,8 @@
 class Course < ApplicationRecord
-  # belongs_to :user
-  has_many :users
+  # has_many :users
+  belongs_to :user
   belongs_to :teacher
+  #do i need to validate the presence of teacher_id down below????
   validates :name, :course_key, :teacher_id, presence: true
   validates_uniqueness_of :name, :course_key
 end
