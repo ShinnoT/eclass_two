f01ab5501f31c4d42d99cc68a9364d6081c5ee93
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-09 01:08:52 +0900

    associations defined for course and user model in association with enrollment model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :teacher
+  has_many :enrollments
+  has_many :users, through: :enrollments
   #do i need to validate the presence of teacher_id down below????
   validates :name, :course_key, :teacher_id, presence: true
   validates_uniqueness_of :name, :course_key
