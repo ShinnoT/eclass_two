fb936539d2e8f7cc72ada9fd1c7dfe591819684f
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-10 23:42:34 +0900

    create method defined

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,2 +1,22 @@
 class EnrollmentsController < ApplicationController
+  def create
+    @course = Course.find(id: params[:id])
+    if enrollment_params == @course.course_key
+      @enrollment = Enrollment.new(enrollment_params)
+      @enrollment.course = @course
+      if @enrollment.save
+        redirect_to course_path(@course)
+      else
+        render 'courses/show'
+      end
+    else
+      render 'courses/show'
+    end
+  end
+
+  private
+
+  def enrollment_params
+    params.require(:enrollment).require(:enrollment_key)
+  end
 end
