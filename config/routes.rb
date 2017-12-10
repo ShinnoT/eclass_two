8469a0623317dd0c9e0d6d343731de13c18091e1
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-10 23:44:05 +0900

    routes configured for enrollments create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :teachers
   devise_for :users
   root to: 'pages#home'
-  resources :courses, only: [:index, :show, :new, :create]
+  resources :courses, only: [:index, :show, :new, :create] do
+    resources :enrollments, only: [:create]
+  end
   # get '/courses/:id/enroll', to: 'courses#enroll'
   # post '/courses/:id/enroll', to: 'courses#enroll', as: 'enroll'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
